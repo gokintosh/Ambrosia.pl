b8ab15eadd383635e9f84e9d52f37476426fa569
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2021-02-26 20:14:11 +0100

    backend initialized

---
+++ backend/src/main/java/com/gokul/ambrosiabackend/AmbrosiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.ambrosiabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbrosiaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmbrosiaBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/gokul/ambrosiabackend/AmbrosiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokul.ambrosiabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbrosiaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
