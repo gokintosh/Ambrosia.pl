3c42eb0ef2d728f4963d7e398c13eac3fcf45447
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2021-02-27 13:35:35 +0100

    learn01

--- backend/src/main/java/com/gokul/ambrosiabackend/controller/TutorialController.java
+++ backend/src/main/java/com/gokul/ambrosiabackend/controller/TutorialController.java
@@ -3,6 +3,7 @@ package com.gokul.ambrosiabackend.controller;
 
 import com.gokul.ambrosiabackend.model.Tutorial;
 import com.gokul.ambrosiabackend.repository.TutorialRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -39,7 +40,7 @@ public class TutorialController {
     }
 
     @GetMapping("/tutorials/{id}")
-    public ResponseEntity<Tutorial>getTutorialByid(@PathVariable("id")long id ){
+    public ResponseEntity<Tutorial>getTutorialById(@PathVariable("id")long id ){
         Optional<Tutorial>tutorialData=tutorialRepository.findById(id);
 
         if(tutorialData.isPresent()){
@@ -57,6 +58,55 @@ public class TutorialController {
             return new ResponseEntity<>(_tutorial,HttpStatus.CREATED);
         }catch (Exception e){
             return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/tutorials/{id}")
+    public ResponseEntity<Tutorial>updateTutorial(@PathVariable("id")long id,@RequestBody Tutorial tutorial){
+        Optional<Tutorial>tutorialData=tutorialRepository.findById(id);
+
+        if(tutorialData.isPresent()){
+            Tutorial _tutorial=tutorialData.get();
+            _tutorial.setTitle(tutorial.getTitle());
+            _tutorial.setDescription(tutorial.getDescription());
+            _tutorial.setPublished(tutorial.isPublished());
+            return new ResponseEntity<>(tutorialRepository.save(_tutorial),HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/tutorials/{id}")
+    public ResponseEntity<HttpStatus>deleteTutorial(@PathVariable("id")long id){
+        try{
+            tutorialRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/tutorials")
+    public ResponseEntity<HttpStatus>deleteAllTutorials(){
+        try {
+            tutorialRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/tutorials/published")
+    public ResponseEntity<List<Tutorial>>getByPublished(){
+        try{
+            List<Tutorial>tutorials=tutorialRepository.findByPublished(true);
+            if(tutorials.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(tutorials,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
