34b28a4401ca3ce5f7c41f55640772f352dae2ec
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2021-02-27 10:00:43 +0100

    testing

---
+++ backend/src/main/java/com/gokul/ambrosiabackend/controller/TutorialController.java
@@ -0,0 +1,63 @@
+package com.gokul.ambrosiabackend.controller;
+
+
+import com.gokul.ambrosiabackend.model.Tutorial;
+import com.gokul.ambrosiabackend.repository.TutorialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class TutorialController {
+
+    @Autowired
+    TutorialRepository tutorialRepository;
+
+    @GetMapping("/tutorials")
+    public ResponseEntity<List<Tutorial>>getAllTutorials(@RequestParam(required=false)String title){
+        try{
+            List<Tutorial>tutorials=new ArrayList<Tutorial>();
+            if(title==null)
+                tutorialRepository.findAll().forEach(tutorials::add);
+            else
+                tutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
+
+            if(tutorials.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(tutorials,HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/tutorials/{id}")
+    public ResponseEntity<Tutorial>getTutorialByid(@PathVariable("id")long id ){
+        Optional<Tutorial>tutorialData=tutorialRepository.findById(id);
+
+        if(tutorialData.isPresent()){
+            return new ResponseEntity<>(tutorialData.get(),HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    @PostMapping("/tutorials")
+    public ResponseEntity<Tutorial>createTutorial(@RequestBody Tutorial tutorial){
+        try{
+            Tutorial _tutorial=tutorialRepository.save(new Tutorial(tutorial.getTitle(),tutorial.getDescription(),false));
+            return new ResponseEntity<>(_tutorial,HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
