68dac54b229eddc73b818123f9da9448d84ae87b
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2021-03-04 10:44:39 +0100

    complete register

--- backend/src/main/java/com/gokul/ambrosiabackend/config/Security.java
+++ backend/src/main/java/com/gokul/ambrosiabackend/config/Security.java
@@ -4,11 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class Security extends WebSecurityConfigurerAdapter {
+
+    UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
