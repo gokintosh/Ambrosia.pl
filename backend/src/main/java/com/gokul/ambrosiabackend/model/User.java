9ce7d8beb3959235a2136d493a56581bc7f5c9e1
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2021-03-08 20:44:42 +0100

    init frontend

--- backend/src/main/java/com/gokul/ambrosiabackend/model/User.java
+++ backend/src/main/java/com/gokul/ambrosiabackend/model/User.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
 import java.time.Instant;
 
 @Data
